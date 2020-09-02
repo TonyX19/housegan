2a2c893755370a1a14b7cbff7ac9c4220f3c9bec
Tony <gump80069@gmail.com>
2020-09-02 22:24:37 +0800

    update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from utils import combine_images_maps, rectangle_renderer
 from models import Discriminator, Generator, compute_gradient_penalty, weights_init_normal
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=100000, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=1000000, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=32, help="size of the batches")
 parser.add_argument("--g_lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--d_lr", type=float, default=0.0001, help="adam: learning rate")
@@ -29,7 +29,7 @@ parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of firs
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=128, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--sample_interval", type=int, default=10, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=50000, help="interval between image sampling")
 parser.add_argument("--exp_folder", type=str, default='exp', help="destination folder")
 parser.add_argument("--n_critic", type=int, default=1, help="number of training steps for discriminator per iter")
 parser.add_argument("--target_set", type=str, default='A', help="which split to remove")
@@ -270,6 +270,6 @@ for epoch in range(opt.n_epochs):
             if (batches_done % opt.sample_interval == 0) and batches_done:
                 torch.save(generator.state_dict(), './checkpoints/{}_{}.pth'.format(exp_folder, batches_done))
                 visualizeSingleBatch(fp_loader_test, opt)
-                
+
             batches_done += opt.n_critic
             
